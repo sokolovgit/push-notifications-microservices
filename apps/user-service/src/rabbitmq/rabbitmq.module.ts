2663451fcb67285e74ba3f5b8cb76f91ef9ab193
sokolovgit <sanok.sokolov928100@gmail.com>
2025-05-15 22:50:34 +0300

    feat(users-service): add rabbitmq client

---
+++ apps/user-service/src/rabbitmq/rabbitmq.module.ts
@@ -0,0 +1,26 @@
+import { Global, Module } from "@nestjs/common"
+import { ConfigModule, ConfigService } from "@nestjs/config"
+import { ClientsModule, Transport } from "@nestjs/microservices"
+
+export const RABBITMQ_SERVICE = "RABBITMQ_SERVICE"
+
+@Global()
+@Module({
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: RABBITMQ_SERVICE,
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>("rabbitmq.url")],
+          },
+        }),
+      },
+    ]),
+  ],
+  exports: [ClientsModule],
+})
+export class RabbitMQModule {}
