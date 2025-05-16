cb22a1a4e26480d8c40670350302345581678ca5
sokolovgit <sanok.sokolov928100@gmail.com>
2025-05-16 03:10:33 +0300

    feat(notification-service): update env example and config

--- apps/notification-service/src/config.ts
+++ apps/notification-service/src/config.ts
@@ -1,6 +1,21 @@
 export default () => ({
+  port: process.env.PORT || 3000,
+
   rabbitmq: {
     url: process.env.RABBITMQ_URL,
     userQueue: process.env.RABBITMQ_USER_QUEUE,
+  },
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+
+  webhook: {
+    uniqueUrl: process.env.WEBHOOK_UNIQUE_URL,
+  },
+
+  bullboard: {
+    enabled: process.env.BULLBOARD_ENABLED === "true",
+    path: process.env.BULLBOARD_PATH || "queues",
   },
 })
